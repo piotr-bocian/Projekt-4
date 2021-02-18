850a71d3cc444c1863e5280bc88d0d7fcf708826
double_s <sz.suchodolski@gmail.com>
2021-02-18 23:45:05 +0100

    add script npm start to package.json - starts nodemon on server.js

--- server.js
+++ server.js
@@ -4,3 +4,5 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
+
+server.listen(port, () => console.log(`Listening on port ${port}...`));
