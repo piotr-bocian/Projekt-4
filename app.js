3293e3b69c84c097a77c5e1e50dcfe85fd5b094e
Łukasz Żurawski <zurawskiu@gmail.com>
2021-02-19 15:47:05 +0100

    added connection to Atlas

--- app.js
+++ app.js
@@ -1,6 +1,21 @@
 const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
 const app = express();
 
-
+mongoose.set('useUnifiedTopology', true);
+mongoose
+  .connect(
+    'mongodb+srv://Lukasz:' +
+      process.env.ANIMAL_SHELTER_PW +
+      '@schronisko.lrx7d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    { useNewUrlParser: true }
+  )
+  .then(() => {
+    console.log('Connected to Atlas MongoDB');
+  })
+  .catch((error) => {
+    console.log('Connection failed', error);
+  });
 
 module.exports = app;
