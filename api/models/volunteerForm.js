3db8c11fb7aa9b201999c8a54158b56a0625a323
Jan Eliasz <johnbg@tlen.pl>
2021-02-22 14:27:43 +0100

    Add regex to phone number

--- api/models/volunteerForm.js
+++ api/models/volunteerForm.js
@@ -5,7 +5,8 @@ const volunteerFormSchema = mongoose.Schema({
     name: {type: String, required: true},
     surname: {type: String, required: true},
     birthDate: {type: Date, required: true},
-    phoneNumber: {type: Number, required: true},
+    phoneNumber: {type: String, required: true, minLength: 11, maxLength: 15, match: /^(\+\d{2} )?\d{3}-\d{3}-\d{3}$/},
+    // mobile no pattern: +48 123-456-789, or 123-456-789
     occupation: {type: String, required: true},
     preferredTasks: {type: String, required: true, enum: ['working with dogs', 'working with cats', 'shelter promotion']}
 });
