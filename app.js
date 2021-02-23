dd5202400cd15a14a36a3abd21c093eaeff6443e
Łukasz Żurawski <zurawskiu@gmail.com>
2021-02-23 20:58:20 +0100

    get and post request work. Problem with Joi validate function.

--- app.js
+++ app.js
@@ -2,21 +2,27 @@ const express = require('express');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const app = express();
+const payment = require('./api/routes/payment');
 
 mongoose.set('useUnifiedTopology', true);
 mongoose
   .connect(
     'mongodb+srv://Lukasz:' +
       process.env.ANIMAL_SHELTER_PW +
-      '@schronisko.lrx7d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-        useNewUrlParser: true,
-        useUnifiedTopology: true 
-    })
+      '@schronisko.lrx7d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
   .then(() => {
     console.log('Connected to Atlas MongoDB');
   })
   .catch((error) => {
     console.log('Connection failed', error);
   });
+
+app.use(express.json());
+app.use('/api/payments', payment);
 
 module.exports = app;
