ab0b0c50ba1b9ee5fa2a3ea13a6dfb2d9bac89ab
Jan Eliasz <johnbg@tlen.pl>
2021-02-23 10:36:12 +0100

    Add volunteer form validation with Joi

--- api/models/volunteerForm.js
+++ api/models/volunteerForm.js
@@ -1,14 +1,64 @@
 const mongoose = require('mongoose');
+const Joi = require('joi');
 
 const volunteerFormSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
-    name: {type: String, required: true},
-    surname: {type: String, required: true},
-    birthDate: {type: Date, required: true},
-    phoneNumber: {type: String, required: true, minLength: 11, maxLength: 15, match: /^(\+\d{2} )?\d{3}-\d{3}-\d{3}$/},
-    // mobile no pattern: +48 123-456-789, or 123-456-789
-    occupation: {type: String, required: true},
-    preferredTasks: {type: String, required: true, enum: ['working with dogs', 'working with cats', 'shelter promotion']}
+    firstName: {
+        type: String,
+        required: true
+    },
+    lastName: {
+        type: String,
+        required: true
+    },
+    birthDate: {
+        type: Date,
+        required: true
+    },
+    mobile: {
+        type: String,
+        required: true,
+        minLength: 11,
+        maxLength: 15,
+        match: /^(\+\d{2} )?\d{3}-\d{3}-\d{3}$/
+        // mobile no pattern: +48 123-456-789, or 123-456-789
+    },
+    occupation: {
+        type: String,
+        required: true
+    },
+    preferredTasks: {
+        type: String,
+        required: true,
+        enum: [
+            'praca z psami', 
+            'praca z kotami', 
+            'promocja schroniska'
+        ]
+    }
 });
 
-module.exports = mongoose.model('VolunteerForm', volunteerFormSchema);
+const VolunteerForm = mongoose.model('VolunteerForm', volunteerFormSchema);
+
+function validateVolunteerForm(volunteerForm){
+    const schema = Joi.object({
+        firstName: Joi.string().required(),
+        lastName: Joi.string().required(),
+        birthDate: Joi.date().required(),
+        mobile: Joi.string().trim().regex(/^(\+\d{2} )?\d{3}-\d{3}-\d{3}$/).required(),
+        occupation: Joi.string.required(),
+        preferredTasks: Joi
+            .string()
+            .valid(
+                'praca z psami',
+                'praca z kotami',
+                'promocja schroniska'
+            )
+            .required()
+    });
+
+    return Joi.validate(volunteerForm, schema);
+}
+
+exports.VolunteerForm = VolunteerForm;
+exports.validateVolunteerForm = validateVolunteerForm;
