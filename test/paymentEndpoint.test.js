3a60aa4995c7f6f3b7946f1c435497f2d44c20a0
Łukasz Żurawski <zurawskiu@gmail.com>
2021-02-27 17:01:57 +0100

    remove consoloe.log

--- test/paymentEndpoint.test.js
+++ test/paymentEndpoint.test.js
@@ -47,123 +47,129 @@ afterAll((done) => {
   done();
 });
 
-// describe('GET /test', function () {
-  it('should get all data from database', function (done) {
-    const post = Payment.create(dummyData);
-    return request(app)
-      .get('/test')
-      .set('Accept', 'application/json')
-      .expect(200)
-      .then((response) => {
-        console.log(response.body.payments);
-        done();
-      })
-      .catch((err) => done(err));
-  });
+// 'GET /test'
+it('should get all data from database', function (done) {
+  const post = Payment.create(dummyData);
+  return request(app)
+    .get('/test')
+    .set('Accept', 'application/json')
+    .expect(200)
+    .then((response) => {
+      expect(response.body.payments.allPaymentsInDatabase).toBe(1);
+      done();
+    })
+    .catch((err) => done(err));
+});
 
-  it('should response with status 400 when incorrect id is sending also it should return: Podano błędny numer _id', function (done) {
-    return request(app)
-      .get('/test/incorectid')
-      .set('Accept', 'application/json')
-      .expect(400, 'Podano błędny numer _id')
-      .then((response) => {
-        console.log(response.text);
-        done();
-      })
-      .catch((err) => done(err));
-  });
+it('should response with status 400 when incorrect id is sending also it should return: Podano błędny numer _id', function (done) {
+  return request(app)
+    .get('/test/incorectid')
+    .set('Accept', 'application/json')
+    .expect(400, 'Podano błędny numer _id')
+    .then((response) => {
+      expect(response.text).toBe('Podano błędny numer _id');
+      done();
+    })
+    .catch((err) => done(err));
+});
 
-  it('should response with status 200 when correct id is sending', function (done) {
-    const post = Payment.create(dummyData2);
-    return request(app)
-      .get('/test/60369dc3e954c736b94a12f9')
-      .set('Accept', 'application/json')
-      .expect(200)
-      .then((response) => {
-        console.log(response.body);
-        done();
-      })
-      .catch((err) => done(err));
-  });
-  it('should response with status 404 when incorrect id is sending, alse it should response with: Płatność, której szukasz nie istnieje', function (done) {
-    return request(app)
-      .get('/test/6incorrectid')
-      .set('Accept', 'application/json')
-      .expect(404, 'Płatność, której szukasz nie istnieje')
-      .then((response) => {
-        console.log(response.body);
-        done();
-      })
-      .catch((err) => done(err));
-  });
-// });
-
-// describe('POST /test', function () {
-  it('should respond with status 201', function (done) {
-    const postData = {
-      typeOfPayment: 'opłata adopcyjna',
-      amount: 25,
-      paymentDate: '2021-07-09',
-      paymentMethod: 'Blik',
-    };
-
-    request(app)
-      .post('/test')
-      .send(postData)
-      .set('Accept', 'application/json')
-      .expect(201)
-      .end(function (err, res) {
-        expect(res.body.message).toBe('Płatność przebiegła pomyślnie');
-        if (err) return done(err);
-        return done();
+it('should response with status 200 when correct id is sending', function (done) {
+  const post = Payment.create(dummyData2);
+  return request(app)
+    .get('/test/60369dc3e954c736b94a12f9')
+    .set('Accept', 'application/json')
+    .expect(200)
+    .then((response) => {
+      expect(response.body.payment).toStrictEqual({
+        _id: '60369dc3e954c736b94a12f9',
+        typeOfPayment: 'opłata adopcyjna',
+        amount: 345,
+        paymentDate: '2021-05-05T00:00:00.000Z',
+        paymentMethod: 'Blik',
+        __v: 0,
       });
-  });
+      done();
+    })
+    .catch((err) => done(err));
+});
+it('should response with status 404 when incorrect id is sending, alse it should response with: Płatność, której szukasz nie istnieje', function (done) {
+  return request(app)
+    .get('/test/6incorrectid')
+    .set('Accept', 'application/json')
+    .expect(404, 'Płatność, której szukasz nie istnieje')
+    .then((response) => {
+      expect(response.text).toBe('Płatność, której szukasz nie istnieje');
+      done();
+    })
+    .catch((err) => done(err));
+});
 
-  it('should respond with status 400 when incorrect data are sended', function (done) {
-    const postData = {
-      typeOfPayment: 'bad data',
-      amount: 25,
-      paymentDate: '2021-07-09',
-      paymentMethod: 'Blik',
-    };
+// 'POST /test'
+it('should respond with status 201', function (done) {
+  const postData = {
+    typeOfPayment: 'opłata adopcyjna',
+    amount: 25,
+    paymentDate: '2021-07-09',
+    paymentMethod: 'Blik',
+  };
 
-    request(app)
-      .post('/test')
-      .send(postData)
-      .set('Accept', 'application/json')
-      .expect(400)
-      .end(function (err, res) {
-        expect(res.text).toBe(
-          '"typeOfPayment" must be one of [opłata adopcyjna, jednorazowy przelew, wirtualny opiekun-opłata cykliczna]'
-        );
-        if (err) return done(err);
-        return done();
-      });
-  });
-// });
+  request(app)
+    .post('/test')
+    .send(postData)
+    .set('Accept', 'application/json')
+    .expect(201)
+    .end(function (err, res) {
+      expect(res.body.message).toBe('Płatność przebiegła pomyślnie');
+      if (err) return done(err);
+      return done();
+    });
+});
 
-  it('should delete data from database', function (done) {
-    const dummyData = {
-      _id: '60369dc3e954c736b94a12f5',
-      typeOfPayment: 'opłata adopcyjna',
-      amount: 19,
-      paymentDate: '2021-05-05',
-      paymentMethod: 'Blik',
-    };
-    const post = Payment.create(dummyData);
-    return request(app)
-      .delete('/test/60369dc3e954c736b94a12f5')
-      .set('Accept', 'application/json')
-      .expect(202)
-      .then((response) => {
-        console.log(response.text);
-        done();
-      })
-      .catch((err) => done(err));
-  });
+it('should respond with status 400 when incorrect data are sended', function (done) {
+  const postData = {
+    typeOfPayment: 'bad data',
+    amount: 25,
+    paymentDate: '2021-07-09',
+    paymentMethod: 'Blik',
+  };
 
+  request(app)
+    .post('/test')
+    .send(postData)
+    .set('Accept', 'application/json')
+    .expect(400)
+    .end(function (err, res) {
+      expect(res.text).toBe(
+        '"typeOfPayment" must be one of [opłata adopcyjna, jednorazowy przelew, wirtualny opiekun-opłata cykliczna]'
+      );
+      if (err) return done(err);
+      return done();
+    });
+});
 
-//you have to clean the collections after the tests
+// DELETE /test
+
+it('should delete data from database', function (done) {
+  const dummyDataDelete = {
+    _id: '60369dc3e954c736b94a12f5',
+    typeOfPayment: 'opłata adopcyjna',
+    amount: 19,
+    paymentDate: '2021-05-05',
+    paymentMethod: 'Blik',
+  };
+  const post = Payment.create(dummyDataDelete);
+  return request(app)
+    .delete('/test/60369dc3e954c736b94a12f')
+    .set('Accept', 'application/json')
+    .expect(404)
+    .then((response) => {
+      // console.log(response.text);
+      expect(response.text).toBe('Płatność, której szukasz nie istnieje');
+      done();
+    })
+    .catch((err) => done(err));
+});
+
 async function removeAllCollections() {
   const collections = Object.keys(mongoose.connection.collections);
   for (const collectionName of collections) {
