8162d228b0fd3c73c10c6d3ec833b1eebcdfa2bd
double_s <sz.suchodolski@gmail.com>
2021-03-02 22:53:11 +0100

    added authorization: loggedUser, isAdmin, isVolunteer

--- api/middleware/authorization.js
+++ api/middleware/authorization.js
@@ -1,6 +1,6 @@
 const jwt = require('jsonwebtoken');
 
-function auth(req, res, next) {
+exports.loggedUser = function (req, res, next) {
     const token = req.header('x-auth-token');
     if (!token) return res.status(401).send('Odmowa dostępu. Operacja możliwa tylko dla zalogowanego użytkownika.');
 
@@ -12,4 +12,14 @@ function auth(req, res, next) {
     catch (ex) {
         res.status(400).send('Nieprawidłowy token.')
     }
+}
+
+exports.isAdmin = function (req, res, next) {
+    if(!req.user.isAdmin) return res.status(403).send('Brak uprawnień do wykonania tej operacji.');
+    next();
+}
+
+exports.isVolunteer = function (req, res, next) {
+    if(!req.user.isVolunteer) return res.status(403).send('Brak uprawnień do wykonania tej operacji.');
+    next();
 }
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,11 +1,13 @@
+const auth = require('../middleware/authorization');
 const { User, validateUser } = require('../models/user');
 const userController = require('../controllers/user');
 const express = require('express');
 const router = express.Router();
 
 
-router.get('/', userController.usersGetAll);
-router.get('/:id', userController.usersGetUser);
+router.get('/me', auth.loggedUser, userController.usersGetMe);
+router.get('/', [auth.loggedUser ,auth.isAdmin], userController.usersGetAll);
+router.get('/:id', [auth.loggedUser ,auth.isAdmin], userController.usersGetUser);
 //don't know why cannot change endpoint addres to /rejestracja.
 router.post('/', userController.usersAddUser);
 
