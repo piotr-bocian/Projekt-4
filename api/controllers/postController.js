6f4b5c6325b500cf8a88f8a7f98be6ec3e210668
Your Name <you@example.com>
2021-03-04 19:06:01 +0100

    POST DEL PUT / problems with tests

--- api/controllers/postController.js
+++ api/controllers/postController.js
@@ -8,7 +8,7 @@ const {Post, ValidatePost} = require('../models/post');
     res.send(postForms);
   }
 
-// GET one post form
+// GET One post form
 exports.postFormGetOne = async (req, res, next) => {
   if(!mongoose.Types.ObjectId.isValid(req.params.postId))
       return res.status(400).send('Podano błędny numer _postId');
@@ -25,79 +25,25 @@ exports.postFormGetOne = async (req, res, next) => {
   });
 }
 
-
-/// NIE SKONCZONE
-/// NIE SKONCZONE
-/// NIE SKONCZONE
-/// NIE SKONCZONE
-
-  // Update post
-exports.editpostForm = async (req, res, next) => {
-     const newPost = await req.body;
-    const postId = Number(req.params.postId);
-    if (!newPost.id || newPost.id !== postId) {
-        newPost.id = postId;
-    }
-    cards[req.cardIndex] = newPost;
-    res.send(newPost);
-  };
-
-
-  // Delete post
-exports.deletepostForm = async (req, res, next) => {
-    await posts.splice(req.postIndex, 1);
-    res.status(204).send();
-  };
-
-  /// Z ADOPTION
-  /// Z ADOPTION
-  /// Z ADOPTION
-  /// Z ADOPTION
-
-// POST adoption form
-exports.addAdoptionForm = async (req, res, next) => {
-  try{
-      const {content, userID, animalID} = req.body;
-      const value = await validateAdoptionForm.validateAsync({
-          content,
-          userID,
-          animalID,
-      });
-      let adoptionFormOne = new AdoptionForm({
-          _id: mongoose.Types.ObjectId(),
-          ...value
-      });
-      adoptionFormOne = await adoptionFormOne.save();
-      res.status(201).send({
-          message: 'Formularz zostal zapisany',
-          adoptionFormOne,
-        });
-  }
-  catch(error){
-      res.status(400).send(error.message);
-  }
-}
-
-// UPDATE adoption form
+// UPDATE post form
 exports.editAdoptionForm = async (req, res, next) => {
-  const adoptionId = req.params.adoptionId;
-  if(!mongoose.Types.ObjectId.isValid(adoptionId))
-      return res.status(400).send('Podano bledny numer _adoptionId');
+  const postId = req.params.postId;
+  if(!mongoose.Types.ObjectId.isValid(postId))
+      return res.status(400).send('Podano bledny numer _postId');
   try{
       const updateOps = {}
       for(const ops of req.body){
           updateOps[ops.propertyName] = ops.newValue;
       }
-      // await validateVolunteerFormLight.validateAsync(updateOps);
-      const adoptionForm = await AdoptionForm.findOneAndUpdate(
-          { _id: id },
+      const postForm = await Post.findOneAndUpdate(
+          { postId: postId },
           { $set: updateOps},
       );
       res.status(200).send({
           message: `Zaktualizowano nastepujące pola ${JSON.stringify(
               updateOps
             )}`,
-            adoptionForm
+            postForm
       });
   }
   catch(error){
@@ -105,19 +51,43 @@ exports.editAdoptionForm = async (req, res, next) => {
   }
 };
 
-// DELETE adoption form
+// DELETE post form
 exports.deleteAdoptionForm = async (req, res, next) => {
-  const adoptionId = req.params.adoptionId;
-  if(!mongoose.Types.ObjectId.isValid(adoptionId))
-      return res.status(400).send('Podano błędny numer _adoptionId');
+  const postId = req.params.postId;
+  if(!mongoose.Types.ObjectId.isValid(postId))
+      return res.status(400).send('Podano błędny numer _postId');
 
-  const adoptionForm = await AdoptionForm.findByIdAndRemove(adoptionId);
+  const postForm = await Post.findByIdAndRemove(postId);
 
-  if(!adoptionForm)
+  if(!postForm)
       return res.status(404).send('Taki formularz nie figuruje w naszej bazie danych');
 
   res.status(202).send({
       message: 'Formularz zostal poprawnie usuniety z bazy danych',
-      adoptionForm
+      postForm
   });
+}
+
+// POST adoption form
+exports.addAdoptionForm = async (req, res, next) => {
+  try{
+      const {content, userID, animalID} = req.body;
+      const value = await Post.validateAsync({
+          postDate,
+          content,
+          photo,
+      });
+      let postForm = new Post({
+        postId: mongoose.Types.ObjectId(),
+          ...value
+      });
+      postForm = await postForm.save();
+      res.status(201).send({
+          message: 'Formularz zostal zapisany',
+          postForm,
+        });
+  }
+  catch(error){
+      res.status(400).send(error.message);
+  }
 }
--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.get('/', postController.postFormsGetAll);
 router.get('/:postId', postController.postFormGetOne);
-// router.post('/', postController.addpostForm);
+router.post('/', postController.addpostForm);
 router.put('/:postId', postController.editpostForm);
 router.delete('/:postId', postController.deletepostForm);
 
