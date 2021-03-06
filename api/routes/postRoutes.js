f53c787893bb16a715984a12777be80e0a125a4a
Your Name <you@example.com>
2021-03-06 18:42:53 +0100

    Merged post/endpoint - added AUTH for USER and ADMIN

--- api/routes/postRoutes.js
+++ api/routes/postRoutes.js
@@ -6,7 +6,7 @@ const auth = require('../middleware/authorization');
 router.get('/', postController.postFormsGetAll);
 router.get('/:postId', postController.postFormGetOne);
 router.post('/', auth.loggedUser, postController.addpostForm);
-router.put('/:postId', auth.isAdmin, postController.editpostForm);
-router.delete('/:postId', auth.isAdmin, postController.deletepostForm);
+router.put('/:postId', [auth.loggedUser, auth.isAdmin], postController.editpostForm);
+router.delete('/:postId', [auth.loggedUser, auth.isAdmin], postController.deletepostForm);
 
 module.exports = router;
